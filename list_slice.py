6a7b8a3e696736164124dcfaa29041e7f1d7037a
Developer-Linus <linusmksu30@gmail.com>
2025-01-07 21:37:15 +0300

    added slicing concept

---
+++ list_slice.py
@@ -0,0 +1,7 @@
+my_list = [10, 20, 39, 45, 50, 54, 60]
+
+print(my_list[2:5]) # Give a sub-list including index 2 and excluding index 5.
+
+print(my_list[::2]) # Gives the result with a step interval of 2.
+
+print(my_list[::-1]) # Reverses the list.
